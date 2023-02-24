47add6a60562bb91dc0aeb4b3336d8285ea0dc60
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2023-02-24 22:42:51 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 9.69 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(t: String, p: String): Int {
+        var answer: Int = 0
+        for (i in 0..t.length-p.length) {
+            if (t.substring(i until i+p.length).toLong() <= p.toLong()) {
+                answer++
+            }
+        }
+        return answer
+    }
+}
