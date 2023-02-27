952e642e143f126546b8b79cfe9e9b022da28c00
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2023-02-27 18:53:59 +0900

    [level 0] Title: 외계행성의 나이, Time: 9.52 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(age: Int) = age.toString()
+        .replace("0","a")
+        .replace("1","b")
+        .replace("2","c")
+        .replace("3","d")
+        .replace("4","e")
+        .replace("5","f")
+        .replace("6","g")
+        .replace("7","h")
+        .replace("8","i")
+        .replace("9","j")
+}
