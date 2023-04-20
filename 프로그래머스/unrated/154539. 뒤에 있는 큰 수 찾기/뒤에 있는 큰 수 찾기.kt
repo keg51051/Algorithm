4ba2a324bde9ce0629459fd6f117ad9d9ef7614a
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2023-04-20 13:35:00 +0900

    [unrated] Title: 뒤에 있는 큰 수 찾기, Time: 60.29 ms, Memory: 172 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.kt
@@ -0,0 +1,15 @@
+import java.util.Stack
+
+class Solution {
+    fun solution(numbers: IntArray): IntArray {
+        val answer = IntArray(numbers.size) { -1 }
+        val stack = Stack<Int>()
+        for (i in numbers.indices) {
+            while (stack.isNotEmpty() && numbers[stack.peek()] < numbers[i]) {
+                answer[stack.pop()] = numbers[i]
+            }
+            stack.push(i)
+        }
+        return answer
+    }
+}
