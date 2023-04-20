fa7a9c6e89ce9efa4d6e1e9f6c667b4adfc8f0b2
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2023-04-20 18:03:23 +0900

    [unrated] Title: 커피 심부름, Time: 6.73 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(order: Array<String>): Int {
+        var answer: Int = 0
+        for (i in order) {
+            when {
+                i.contains("cafelatte") -> answer += 5000
+                else -> answer += 4500
+            }
+        }
+        return answer
+    }
+}
