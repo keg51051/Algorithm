ca6dd4975c51083b5f536b84caee35476a0f9a4d
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2023-04-22 20:48:33 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(numbers: IntArray, n: Int): Int {
+        var answer: Int = 0
+        var i = 0
+        while (answer <= n) {
+            answer += numbers[i]
+            i++
+        }
+        return answer
+    }
+}
