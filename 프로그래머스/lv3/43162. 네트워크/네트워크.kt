2f0bb3aa899d116b61dce61237b97b8a1167ac1e
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2023-04-28 22:45:53 +0900

    [level 3] Title: 네트워크, Time: 0.18 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.kt
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.kt
@@ -1,21 +1,21 @@
 class Solution {
-    val disconnected = mutableListOf<Int>()
     fun solution(n: Int, computers: Array<IntArray>): Int {
         var answer = 0
-        for (i in 0 until n) disconnected.add(i)
-        
-        while (disconnected.size > 0) {
-            answer++
-            dfs(n, computers, disconnected.removeAt(0))
+        val visited = BooleanArray(n)
+        for (i in computers.indices) {
+            if (!visited[i]) {
+                dfs(computers, visited, i)
+                answer++
+            }
         }
         return answer
     }
-    
-    fun dfs(n: Int, computers: Array<IntArray>, computer: Int) {
-        for (i in 0 until n) {
-            if (disconnected.contains(i) && computers[computer][i] == 1) {
-                disconnected.remove(i)
-                dfs(n, computers, i)
+
+    private fun dfs(computers: Array<IntArray>, visited: BooleanArray, node: Int) {
+        visited[node] = true
+        computers[node].forEachIndexed { index, value ->
+            if (value == 1 && !visited[index]) {
+                dfs(computers, visited, index)
             }
         }
     }
