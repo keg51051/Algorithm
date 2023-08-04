dc9a223c65a1c06fcb9df9d34cd1d67629e17656
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2023-08-04 10:18:47 +0900

    [Silver III] Title: N과 M (1), Time: 352 ms, Memory: 32656 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
@@ -0,0 +1,31 @@
+import kotlin.text.StringBuilder
+
+private var n = 0
+private var m = 0
+val arr = arrayListOf<Int>()
+val sb = StringBuilder()
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val nm = readLine().split(" ").map { it.toInt() }
+    n = nm[0]
+    m = nm[1]
+    dfs(1, 0)
+    println(sb)
+}
+
+fun dfs(start: Int, depth: Int) {
+    if (depth == m) {
+        for (i in arr) {
+            sb.append(i).append(' ')
+        }
+        sb.append('\n')
+        return
+    }
+
+    for (i in 1..n) {
+        if (arr.contains(i)) continue
+        arr.add(i)
+        dfs(i + 1, depth + 1)
+        arr.removeLast()
+    }
+}
