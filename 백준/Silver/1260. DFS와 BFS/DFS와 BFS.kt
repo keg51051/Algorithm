d24ad28d8b8ea9c81d93a882b13b5d15e739eb07
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2023-11-22 15:28:31 +0900

    [Silver II] Title: DFS와 BFS, Time: 332 ms, Memory: 28672 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.kt
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.kt
@@ -1,18 +1,16 @@
 import java.io.BufferedReader
-import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.util.LinkedList
-import kotlin.collections.mutableListOf
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
 
-    val (n,m,v) = br.readLine().split(" ").map { it.toInt() }
+    val (n, m, v) = br.readLine().split(" ").map { it.toInt() }
     val nodes = MutableList(n+1) { mutableListOf<Int>() }
     var visited = BooleanArray(n+1)
 
     repeat(m) {
-        val (a,b) = br.readLine().split(" ").map { it.toInt() }
+        val (a, b) = br.readLine().split(" ").map { it.toInt() }
         nodes[a].add(b)
         nodes[b].add(a)
     }
@@ -21,6 +19,7 @@ fun main() {
 
     dfs(nodes, visited, v)
     visited = BooleanArray(n+1)
+    println()
     bfs(nodes, visited, v)
 
     br.close()
@@ -29,6 +28,7 @@ fun main() {
 fun dfs(nodes: MutableList<MutableList<Int>>, visited: BooleanArray, node: Int) {
     visited[node] = true
     print("$node ")
+
     for (i in nodes[node]) {
         if (!visited[i]) {
             dfs(nodes, visited, i)
@@ -40,7 +40,6 @@ fun bfs(nodes: MutableList<MutableList<Int>>, visited: BooleanArray, node: Int) 
     val q = LinkedList<Int>()
     q.offer(node)
     visited[node] = true
-    println()
 
     while(q.isNotEmpty()) {
         val temp = q.poll()
