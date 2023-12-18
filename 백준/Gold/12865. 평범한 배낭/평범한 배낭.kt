f80a5b90f9039b1712c6c5e4374f172e6dde14fa
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2023-12-18 12:21:33 +0900

    [Gold V] Title: 평범한 배낭, Time: 272 ms, Memory: 25932 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.kt
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.kt
@@ -4,28 +4,23 @@ import java.io.InputStreamReader
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val (N, K) = br.readLine().split(" ").map { it.toInt() }
-    val items = Array(N+1) { IntArray(2) }
-    val dp = Array(N+1) { IntArray(K+1) }
+    val items = Array(N) { IntArray(2) }
+    val dp = IntArray(K+1)
 
-    for (i in 1..N) {
+    for (i in 0..<N) {
         val item = br.readLine().split(" ").map { it.toInt() }.toIntArray()
         items[i] = item
     }
 
-    for (i in 1..N) {
-        for (j in 1..K) {
-            val itemW = items[i][0]
-            val itemV = items[i][1]
-
-            if (j >= itemW) {
-                dp[i][j] = maxOf(dp[i-1][j-itemW] + itemV, dp[i-1][j])
-            } else {
-                dp[i][j] = dp[i-1][j]
+    items.forEach {
+        for (i in K downTo 0) {
+            if (it[0] <= i) {
+                dp[i] = maxOf(dp[i], dp[i - it[0]] + it[1])
             }
         }
     }
-
-    print(dp[N][K])
+    
+    print(dp.last())
 
     br.close()
 }
