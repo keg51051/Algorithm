62656ba3e9272e3159b97c51edf9fa92072e59d3
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2023-12-20 16:17:58 +0900

    [Gold V] Title: 동전 1, Time: 140 ms, Memory: 20556 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.kt
+++ 백준/Gold/2293. 동전 1/동전 1.kt
@@ -5,19 +5,14 @@ fun main() {
 
     val br = BufferedReader(InputStreamReader(System.`in`))
     val (n, k) = br.readLine().split(" ").map { it.toInt() }
-    var values = intArrayOf()
-    val dp = IntArray(k+1) {0}
+    val dp = IntArray(k+1)
 
     dp[0] = 1
 
     repeat(n) {
         val v = br.readLine().toInt()
-        values += v
-    }
-
-    for (i in values) {
-        for (j in i..k) {
-            dp[j] += dp[j-i]
+        for (i in v..k) {
+            dp[i] += dp[i-v]
         }
     }
 
