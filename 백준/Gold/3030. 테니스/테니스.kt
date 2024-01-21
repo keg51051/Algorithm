48df1b40bb5a5a1a356fcdafb66e687c7754b746
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2024-01-21 16:24:04 +0900

    [Gold II] Title: 테니스, Time: 132 ms, Memory: 20724 KB -BaekjoonHub

--- 백준/Gold/3030. 테니스/테니스.kt
+++ 백준/Gold/3030. 테니스/테니스.kt
@@ -1,3 +1,6 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 fun checkOneSet(
     g1: Int,
     g2: Int,
@@ -25,42 +28,28 @@ fun checkOneSet(
     if (game1 < 6) return false
     if (game1 == 6) return game2 <= 4
     if (game1 == 7 && setNumber < 3) return game2 == 5 || game2 == 6
-    if (game1 >= 7 && setNumber == 3) return game1 == game2 + 2
+    if (setNumber == 3) return game1 == game2 + 2
 
     return false
 }
 
 fun main() {
-    val (ime1, ime2) = readln().split(" ")
-    val n = readln().toInt()
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (ime1, ime2) = br.readLine().split(" ")
+    val n = br.readLine().toInt()
 
     repeat(n) {
         var ok = true
-        val red = readln()
-        var p = 0
+        val games = br.readLine().split(" ")
         val pob = intArrayOf(0, 0)
-        var kojiSet: Int
-        var g1: Int
-        var g2: Int
-        var ucitano: Int
 
-        for (kojiSet in 1..red.count { it == ':' }) {
-            val result = Regex("""(\d+:\d+)""").find(red.substring(p))
-            if (result != null) {
-                val (score, _) = result.destructured
-                val scores = score.split(":")
-                g1 = scores[0].toInt()
-                g2 = scores[1].toInt()
-                p += score.length
-                ok = ok && pob[0] < 2 && pob[1] < 2 && checkOneSet(g1, g2, kojiSet, ime1, ime2, pob)
-            } else {
-                break
-            }
+        for (i in games.indices) {
+            val game = games[i]
+            val (g1, g2) = game.split(":").map { it.toInt() }
+            ok = ok && pob[0] < 2 && pob[1] < 2 && checkOneSet(g1, g2, i+1, ime1, ime2, pob)
         }
 
         ok = ok && (pob[0] == 2 || pob[1] == 2)
         println(if (ok) "da" else "ne")
     }
 }
-
-
