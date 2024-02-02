123f61fbe8a3ffa3b2447c1979b4f28ae45a47de
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2024-02-02 18:40:50 +0900

    [Gold V] Title: 적록색약, Time: 100 ms, Memory: 13328 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.kt
@@ -0,0 +1,61 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    val grid = Array(n) { CharArray(n) }
+    var visited = Array(n) { BooleanArray(n) }
+    val dx = intArrayOf(0, 0, 1, -1)
+    val dy = intArrayOf(1, -1, 0, 0)
+
+    repeat(n) {
+        val l = br.readLine().toCharArray()
+        grid[it] = l
+    }
+
+    var section = 0
+
+    fun dfs(x: Int, y: Int): Boolean {
+        if (visited[x][y]) {
+            return false
+        }
+
+        visited[x][y] = true
+
+        for (d in 0..<4) {
+            val nx = x + dx[d]
+            val ny = y + dy[d]
+
+            if (nx in 0..<n && ny in 0..<n && grid[nx][ny] == grid[x][y]) {
+                dfs(nx, ny)
+            }
+        }
+
+        return true
+    }
+
+
+    for (i in 0..<n) {
+        for (j in 0..<n) {
+            if(dfs(i, j)) section += 1
+            // make the grid color blindness R => G or G => R
+            if (grid[i][j] == 'R') grid[i][j] = 'G' 
+        }
+    }
+
+    print("$section ") // color blindness X
+
+    // reset
+    section = 0
+    visited = Array(n) { BooleanArray(n) }
+
+    for (i in 0..<n) {
+        for (j in 0..<n) {
+            if(dfs(i, j)) section += 1
+        }
+    }
+
+    print(section) // color blindness O
+
+}
