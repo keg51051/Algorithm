df6ec9d0b1737ae5f4801849d69c81eb8b85c3d1
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2024-02-07 15:38:19 +0900

    [Gold V] Title: 기차 여행, Time: 2568 ms, Memory: 101328 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10713. 기차 여행/기차 여행.kt
+++ 백준/Gold/10713. 기차 여행/기차 여행.kt
@@ -5,36 +5,28 @@ fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val (n, m) = br.readLine().split(" ").map { it.toInt() }
     val cities = br.readLine().split(" ").map { it.toInt() }
-    val prices = Array(n - 1) { IntArray(3) }
-    val trip = mutableMapOf<Int, Int>()
-    var total = 0
+    val prices = Array(n - 1) { LongArray(3) }
+    val trip = IntArray(n-1)
+    var total: Long = 0
 
     repeat(n - 1) {
-        prices[it] = br.readLine().split(" ").map { it.toInt() }.toIntArray()
+        prices[it] = br.readLine().split(" ").map { p -> p.toLong() }.toLongArray()
     }
 
-    for (i in 0..<cities.size - 1) {
-        if (cities[i] < cities[i+1]) {
-            for (j in cities[i]..<cities[i+1]) {
-                trip[j] = trip.getOrDefault(j, 0) + 1
-            }
-        } else {
-            for (j in cities[i]-1 downTo cities[i+1]) {
-                trip[j] = trip.getOrDefault(j, 0) + 1
-            }
+    // count how many trips
+    for (i in 0..<m-1) {
+        val start = minOf(cities[i], cities[i + 1])
+        val end = maxOf(cities[i], cities[i + 1])
+
+        for (j in start ..< end) {
+            trip[j - 1] += 1
         }
     }
 
-    for (i in trip) {
-        val ticket = prices[i.key - 1][0]
-        val ic = prices[i.key - 1][1]
-        val card = prices[i.key - 1][2]
-
-        total += if (ticket * i.value < card + ic * i.value) {
-            ticket * i.value
-        } else {
-            card + ic * i.value
-        }
+    // compare (ticket * count), (card + ic * count)
+    for (i in trip.indices) {
+        total += minOf(prices[i][0] * trip[i].toLong(),
+            prices[i][2] + prices[i][1] * trip[i].toLong())
     }
 
     print(total)
