69b421824a3017111a9d43acf41f724d078933b1
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2024-02-07 23:19:41 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 684 ms, Memory: 42360 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.kt
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.kt
@@ -4,33 +4,26 @@ import java.io.InputStreamReader
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    val arr = Array(n) { IntArray(m) }
-    val dp = Array(n) { IntArray(m) }
+    // 자연수 입력을 위한 n+1 * m+1 배열
+    val arr = Array(n + 1) { IntArray(m + 1) }
+    val dp = Array(n + 1) { IntArray(m + 1) }
 
-    for (i in arr.indices) {
-        arr[i] = br.readLine().split(" ").map { it.toInt() }.toIntArray()
+    for (i in 1..n) {
+        val l = br.readLine().split(" ").map { it.toInt() }
+        for (j in 1..m) {
+            arr[i][j] = l[j - 1]
+        }
     }
 
-    for (i in 0..<n) {
-        dp[i][0] = arr[i][0]
-        for (j in 1..<m) {
-            dp[i][j] = dp[i][j - 1] + arr[i][j]
+    for (i in 1..n) {
+        for (j in 1..m) {
+            dp[i][j] = dp[i][j - 1] + dp[i - 1][j] + arr[i][j] - dp[i - 1][j - 1]
         }
     }
 
     repeat(br.readLine().toInt()) {
         val (i, j, x, y) = br.readLine().split(" ").map { it.toInt() }
-        var sum = 0
 
-        for (row in i - 1..<x) {
-            sum += if (j - 2 >= 0) {
-                dp[row][y - 1] - dp[row][j - 2]
-            } else {
-                dp[row][y - 1]
-            }
-        }
-
-        println(sum)
+        println(dp[x][y] - dp[x][j - 1] - dp[i - 1][y] + dp[i - 1][j - 1])
     }
-
 }
