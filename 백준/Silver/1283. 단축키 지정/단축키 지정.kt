9ffec6dec87cce7d8797aca2baa7672a448cc727
Hanseong Kim <53955564+keg51051@users.noreply.github.com>
2024-03-30 00:51:53 +0900

    [Silver I] Title: 단축키 지정, Time: 160 ms, Memory: 21628 KB -BaekjoonHub

---
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.kt
@@ -0,0 +1,32 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    val set = mutableSetOf<Char>()
+
+    loop@ for (i in 0..<n) {
+        val command = br.readLine().split(" ").toMutableList()
+
+        for (j in command.indices) {
+            if (set.add(command[j][0].lowercaseChar())) {
+                command[j] = "[${command[j][0]}]${command[j].substring(1)}"
+                println(command.joinToString(" "))
+                continue@loop
+            }
+        }
+
+        for (j in command.indices) {
+            for (k in command[j].indices) {
+                if (set.add(command[j][k].lowercaseChar())) {
+                    command[j] = "${command[j].substring(0..<k)}[${command[j][k]}]${command[j].substring(k+1)}"
+                    println(command.joinToString(" "))
+                    continue@loop
+                }
+            }
+        }
+
+        println(command.joinToString(" "))
+    }
+}
